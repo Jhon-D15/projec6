1f61b154711f9b0273ee96da39cd4602c3ffefce
Jhon-D15 <149914828+Jhon-D15@users.noreply.github.com>
2023-11-16 14:03:55 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,27 @@
 import java.util.Scanner;
 
-public class main.java {
-    public static void main(String[] args) {
+public class longitud {
+
+public static void main(String[] args) {
         Scanner lector = new Scanner(System.in);
 
-  double radio;
-  double longitud; 
-  double area;
+        double radio;
+        double length;
+        double area;
+
+        System.out.println("///////////////////////////////////////////////////////////////////////");
 
         System.out.println("debes ingresar una medida para poder calcular el tamaño del radio");
         radio = lector.nextDouble();
 
 
-        longitud = (2*radio*3.1416);
-        area = ((longitud*radio)/2);
+        length = (2*radio*3.1416);
+        area = ((length*radio)/2);
+
+        System.out.println("///////////////////////////////////////////////////////////////////////");
 
         System.out.println(" area total del circulo es:"+" "+area+" "+"cm^2");
-        System.out.println("la longitud del circulo es:"+" "+longitud+" "+"cm^3");
+        System.out.println("la longitud del circulo es:"+" "+length+" "+"cm^3");
 
-    }
 }
+        }
